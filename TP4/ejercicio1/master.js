341f3b7133bf2564bcd303b02cba74a68d011f47
floyd144 <nelsoncarton@gmail.com>
2017-10-13 17:43:04 -0300

    agrego ejemplo monstruo, empiezo tp especial movimiento de personaje

---
+++ TP4/ejercicio1/master.js
@@ -0,0 +1,12 @@
+let angule=0;
+let transX=200;
+let transY=200;
+var scale=0.5;
+let getEl=  document.getElementById("div1").style;
+function rotar(){
+
+//getEl.transform="rotate("+(angule+=10)+"deg)";
+//getEl.transform="translate("+(transX+=100)+"px, "+(transY+=50)+"px)";
+getEl.transform="scale("+(scale)+")";
+
+}
