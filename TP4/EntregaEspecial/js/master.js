b602501370fd454a0430861705634023ebc5b2aa
floyd144 <nelsoncarton@gmail.com>
2017-10-22 00:49:44 -0300

    implemento movimiento saltar, cambios de animaciones mediante js, modificacion de imagen a fondo transparente

--- TP4/EntregaEspecial/js/master.js
+++ TP4/EntregaEspecial/js/master.js
@@ -1,6 +1,25 @@
-let getEl=  document.getElementById("bugs").style;
-function cambio(){
-  console.log("rr");
-getEl.animation="jump(" +(0.8)+"s)" + "(steps"+(6)+")infinite;";
+$( document ).ready(function() {
 
-}
+
+
+
+
+});
+
+  let url="url(images/tinyBugs.png)";
+  let animationName="run";
+  let steps="steps(6)";
+
+
+  function correr(){
+  $("#bugs").css("background",url);
+  $("#bugs").css("animation-name",animationName);
+  $("#bugs").css("animation-timing-function",steps );
+  } 
+
+
+  let backStop="none";
+  let backMove="backMove"
+  function setBackMove(){
+    $("#fondoDelante,#fondoLejano").css("animation-name",backMove);
+  }
